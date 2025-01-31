7d10b652c09b5e938fc3d304f98310cd5312b3b4
Basel <basil.omsha@gmail.com>
2025-01-31 12:45:36 +0200

    Wrap main view in SafeAreaView

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar"
+import React from "react"
 
-import { StyleSheet, Text, View } from "react-native"
+import { SafeAreaView, StyleSheet, Text, View } from "react-native"
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Hello olivia</Text>
-            <StatusBar style="auto" />
-        </View>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Text>Hello olivia</Text>
+                <StatusBar style="auto" />
+            </View>
+        </SafeAreaView>
     )
 }
 
