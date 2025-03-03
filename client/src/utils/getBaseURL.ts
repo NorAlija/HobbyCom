5cf83418eda913231453ac225bce402bec9d3a52
Basel <basil.omsha@gmail.com>
2025-03-04 00:07:21 +0200

    Refactor getBaseURL function to simplify development mode URL handling and remove platform-specific code

--- client/src/utils/getBaseURL.ts
+++ client/src/utils/getBaseURL.ts
@@ -1,6 +1,5 @@
 import { ExtraConfig } from "@/types"
 import Constants from "expo-constants"
-import { Platform } from "react-native"
 
 export default function getBaseURL() {
     const extra = Constants.expoConfig?.extra as ExtraConfig
@@ -11,9 +10,9 @@ export default function getBaseURL() {
     const mode = extra.mode
 
     if (mode === "development") {
-        if (Platform.OS === "android") {
-            return __DEV__ ? "http://10.0.2.2:PORT" : extra.apiUrl.development.network
-        }
+        // if (Platform.OS === "android") {
+        //     return __DEV__ ? "http://10.0.2.2:PORT" : extra.apiUrl.development.network
+        // }
         // return __DEV__ ? extra.apiUrl.development.local : extra.apiUrl.development.network
         return __DEV__ ? extra.apiUrl.development.network : undefined
     }
